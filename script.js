d80c5d118178a6f803f9127ee97df71595bc034e
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2024-03-25 22:40:51 -0300

    click event

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ document.onmousemove = (event) => {
 
   pupil.style.transform = `translate( -${x}, -${y}`;
 };
+
+function handleClick(event) {
+  const x = (event.clientX * 100) / window.innerWidth;
+  const y = (event.clientY * 100) / window.innerHeight;
+
+  console.log(`Clique na Posição: ${x}, ${y}`);
+}
+
+window.addEventListener("click", handleClick);
